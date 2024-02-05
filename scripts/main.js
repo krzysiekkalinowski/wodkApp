73d465193e404b855e6fdec0cfa08309137c7f81
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-02-05 21:06:22 +0100

    added JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,23 @@ const removeButton = document.querySelector('.remove-button');
 const imgCounter = document.querySelector('.img-counter');
 const vodkaCounter = document.querySelector('.vodka-counter');
 const caloriesCounter = document.querySelector('.calories-counter');
+
+const updateCounter = () => {
+    imgCounter.innerHTML = localStorage.length;
+    vodkaCounter.innerHTML = localStorage.length;
+    caloriesCounter.innerHTML = (localStorage.length * 231);
+}
+
+addButton.addEventListener('click', () => {
+    const glassNumber = localStorage.length;
+    localStorage.setItem(`Glass nr ${glassNumber + 1}`, `${(glassNumber + 1)}`);
+    updateCounter();
+})
+
+removeButton.addEventListener('click', () => {
+    const glassNumber = localStorage.length;
+    localStorage.removeItem(`Glass nr ${glassNumber}`);
+    updateCounter();
+});
+
+updateCounter();
