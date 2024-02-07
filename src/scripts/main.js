db4877841d657f38dd41d451ea437d570b5538f8
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-02-07 22:14:03 +0100

    added history func in localStorage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,25 +5,34 @@ const vodkaCounter = document.querySelector('.vodka-counter');
 const caloriesCounter = document.querySelector('.calories-counter');
 
 let glasses = 0;
-let calories = (glasses * 116);
+let lastCalories = 0;
+
+const key = new Date().toISOString().slice(0, 10);
 
 const updateCounter = (value) => {
     imgCounter.innerHTML = value;
     vodkaCounter.innerHTML = value;
+    let calories = (value * 116);
+    lastCalories = calories;
     caloriesCounter.innerHTML = calories;
-}
+};
 
+const updateHistory = () => {
+    localStorage.setItem(`${key}:`, `Ilość kieliszków: ${glasses}, Ilość kalorii: ${lastCalories}`)
+};
 
 updateCounter(glasses);
 
 addButton.addEventListener('click', () => {
     glasses = glasses + 1;
     updateCounter(glasses);
-})
+    updateHistory();
+});
 
 removeButton.addEventListener('click', () => {
     if (glasses >=1) {
         glasses = glasses - 1;
         updateCounter(glasses);
+        updateHistory();
     };
 });
